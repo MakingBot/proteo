bf5090cb12ba4722d7f720b4313513707424ec58
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-04-16 16:35:11 +0200

    Create Container.hpp

---
+++ proteo/core/Container.hpp
@@ -0,0 +1,67 @@
+#ifndef CONTAINER_HPP
+#define CONTAINER_HPP
+//!
+//! \file Container.hpp
+//!
+// Copyright 2015 MakingBot
+// This file is part of proteo.
+//
+// proteo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// proteo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with proteo.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace proteo { namespace core {
+
+class Container : public Object
+{
+
+public:
+
+    //! \brief Default constructo
+    //!
+    Container(std::string name);
+
+    // ========================================================================
+    // => Constantes
+
+    //! \brief Role
+    //!
+    static const core::ObjRole ORole;
+
+    //! \brief Tag
+    //!
+    static const core::TagArray OTag;
+
+    //! \brief Property vector
+    //!
+    static const std::vector<core::Property> Properties;
+
+    // ========================================================================
+    // => Object redefinition
+
+    //! \brief FROM Object
+    //!
+    virtual core::ObjRole objRole() const;
+
+    //! \brief FROM Object
+    //!
+    virtual const core::TagArray& objTag() const;
+
+    //! \brief FROM Object
+    //!
+    virtual const std::vector<core::Property>& properties() const;
+
+};
+
+} // core
+} // proteo
+#endif // CONTAINER_HPP
