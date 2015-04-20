317e1ab59b7e8f68393c0882013af0cdd366182e
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-04-20 16:42:56 +0200

    Update BhConditionedEvent.hpp

--- proteo/brain/BhConditionedEvent.hpp
+++ proteo/brain/BhConditionedEvent.hpp
@@ -19,41 +19,80 @@
 // You should have received a copy of the GNU General Public License
 // along with proteo.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include "BhEvent.hpp"
 
 namespace proteo { namespace brain {
 
-
+//! \class BhConditionedEvent
+//! \brief
+//!
+//! \author [XR]MakingBot ( http://makingbot.fr )
+//!
 class BhConditionedEvent : public BhEvent
 {
 
 public:
 
+    //! \brief Default constructor
+    //!
     inline BhConditionedEvent(const boost::python::object& cond);
 
-
+    //! \brief 
+    //!
     inline const boost::python::object& condition();
+
+
+    inline void check();
 
 protected:
 
+    bool m_triggered;
+
+    //! \brief 
+    //!
     boost::python::object m_condition;
 
 };
 
-
+/* ============================================================================
+ *
+ * */
 inline BhConditionedEvent::BhConditionedEvent(const boost::python::object& cond)
     : BhEvent()
 {
     m_condition = cond;
 }
 
-
+/* ============================================================================
+ *
+ * */
 inline const boost::python::object& BhConditionedEvent::condition()
 {
     return m_condition;
 }
 
+/* ============================================================================
+ *
+ * */
+inline void BhConditionedEvent::check()
+{
+    // boost::python::call<bool>(m_condition.ptr())
+    if( m_condition() )
+    {
+        if(!m_triggered)
+        {
+            m_action();
+            m_triggered = true;
+        }
+    }
+    else
+    {
+        if(m_triggered)
+        {
+            m_triggered = false;
+        }
+    }
+}
 
 } // brain
 } // proteo
