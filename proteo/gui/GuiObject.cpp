317fab7da71dfd335c3ca4eddb2491d5d9ef5b3d
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-04-22 15:16:20 +0200

    Create GuiObject.cpp

---
+++ proteo/gui/GuiObject.cpp
@@ -0,0 +1,45 @@
+//!
+//! \file GuiObject.cpp
+//!
+// Copyright 2015 MakingBot
+// This file is part of proteo.
+//
+// proteo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// proteo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with proteo.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+/* ============================================================================
+ *
+ * */
+GuiObject::GuiObject(std::string name)
+    : core::Object(name)
+{
+
+}
+
+/* ============================================================================
+ *
+ * */
+bool GuiObject::isVisible()
+{
+    return m_visible;
+}
+
+/* ============================================================================
+ *
+ * */
+GuiObject::setVisible(bool v)
+{
+    m_visible = v;
+}
