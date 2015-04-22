1651f514baf647c007c5102db4f275e9d0c3cfbc
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-04-22 15:19:13 +0200

    Create Composer.hpp

---
+++ proteo/gui/Composer.hpp
@@ -0,0 +1,87 @@
+#ifndef COMPOSER_HPP
+#define COMPOSER_HPP
+//!
+//! \file Composer.hpp
+//!
+// Copyright 2015 MakingBot
+// This file is part of proteo.
+//
+// proteo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// proteo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with proteo.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <proteo/core/Object.hpp>
+
+namespace proteo { namespace gui {
+
+class ComposerWidget;
+
+//! \class
+//!
+class Composer : public core::Object
+{
+
+public:
+    
+    //! \brief Default constructor
+    //!
+    Composer(std::string name);
+
+
+    // ========================================================================
+    // => Constantes
+
+    //! \brief Role
+    //!
+    static const core::ObjRole ORole;
+
+    //! \brief Tag
+    //!
+    static const core::Object::TagArray OTag;
+
+    //! \brief Property vector
+    //!
+    static const std::vector<core::Property> Properties;
+
+    // ========================================================================
+    // => Object redefinition
+
+    //! \brief FROM Object
+    //!
+    virtual core::ObjRole objRole() const;
+
+    //! \brief FROM Object
+    //!
+    virtual const core::Object::TagArray& objTag() const;
+
+    //! \brief FROM Object
+    //!
+    virtual const std::vector<core::Property>& properties() const;
+
+
+protected:
+
+    //! \brief The widget
+    //!
+    // boost::shared_ptr<ComposerWidget> m_widget;
+
+    //! \brief Container that is composed
+    //!
+    boost::shared_ptr<core::Object> m_container;
+
+};
+
+
+} // gui
+} // proteo
+#endif // COMPOSER_HPP
+
