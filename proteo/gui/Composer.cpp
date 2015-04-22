9893e09f75f5a5d3aace4d4ca8c5b5a4ffa6f1c9
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-04-22 15:18:46 +0200

    Create Composer.cpp

---
+++ proteo/gui/Composer.cpp
@@ -0,0 +1,77 @@
+//!
+//! \file Composer.cpp
+//!
+// Copyright 2015 MakingBot
+// This file is part of proteo.
+//
+// proteo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// proteo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with proteo.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <proteo/gui/Composer.hpp>
+// #include <proteo/gui/Composer/ComposerWidget.hpp>
+
+using namespace proteo;
+using namespace proteo::gui;
+using namespace proteo::core;
+
+/* ============================================================================
+ *
+ * */
+Composer::Composer(std::string name)
+    : Object(name)
+    //, m_widget(new ComposerWidget())
+{
+    //m_widget->show();
+}
+
+
+/* ============================================================================
+ *
+ * */
+const ObjRole Composer::ORole = Rgui;
+
+/* ============================================================================
+ *
+ * */
+const Object::TagArray Composer::OTag = { 'C', 'O', 'M', 'P' };
+
+/* ============================================================================
+ *
+ * */
+const std::vector<Property> Composer::Properties = {
+   // Property()
+};
+
+/* ============================================================================
+ *
+ * */
+ObjRole Composer::objRole() const
+{
+    return ORole;
+}
+
+/* ============================================================================
+ *
+ * */
+const Object::TagArray& Composer::objTag() const
+{
+    return OTag;
+}
+
+/* ============================================================================
+ *
+ * */
+const std::vector<Property>& Composer::properties() const
+{
+    return Properties;
+}
