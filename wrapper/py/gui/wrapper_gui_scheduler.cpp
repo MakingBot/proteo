6020eacb7c5121c032b89ee179c28c167d41d14d
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2015-04-23 11:01:35 +0200

    Create wrapper_gui_scheduler.cpp

---
+++ wrapper/py/gui/wrapper_gui_scheduler.cpp
@@ -0,0 +1,41 @@
+//!
+//! \file wrapper_gui_scheduler.cpp
+//!
+// Copyright 2015 MakingBot
+// This file is part of proteo.
+//
+// proteo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// proteo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with proteo.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <boost/python.hpp>
+#include <proteo/gui/Scheduler.hpp>
+
+using namespace boost::python;
+using namespace proteo::core;
+using namespace proteo::gui;
+
+/* ============================================================================
+ *
+ * */
+void export_gui_scheduler()
+{
+    class_<Scheduler, boost::shared_ptr<Scheduler>, bases<Object>, boost::noncopyable>
+    (
+        "Scheduler"
+    ,   init<std::string>()
+    )
+    .def("start", &Scheduler::start)
+
+    ;
+}
+
